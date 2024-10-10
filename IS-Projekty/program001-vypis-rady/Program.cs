da6708c87bccc45e9bc730ac5a643873945f7a54
Lucie Matějková <lucie.matejkova@tul.cz>
2024-10-10 14:38:44 +0200

    #1 program vypis rady

--- IS-Projekty/program001-vypis-rady/Program.cs
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -16,9 +16,45 @@ class Program {
             
             //vstup od uživatele - TO-DO
 
-            //logika pro výpis řady - TO-DO
+            //vstup od uživatele - špatná varianta
+            //Console.Write("Zadejte první číslo řady: ");
+            //int first = int.Parse(Console.ReadLine());
 
-            //opakování programu
+            //vstup od uživatele - lepší varianta
+            Console.Write("Zadejte první číslo řady (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(),out first)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celé číslo): ");
+            }
+            
+            Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+            int last;
+            while(!int.TryParse(Console.ReadLine(),out last)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu poslední číslo řady (celé číslo): ");
+            }
+
+            Console.Write("Zadejte diference (celé číslo): ");
+            int step;
+            while(!int.TryParse(Console.ReadLine(),out step)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu diference (celé číslo): ");
+            }
+
+            // výpis uživatelského vstupu
+            Console.WriteLine();
+            Console.WriteLine("===================");
+            Console.WriteLine("První číslo řady: {0}", first);
+            Console.WriteLine("Poslední číslo řady: {0}", last);
+            Console.WriteLine("Diference: {0}", step);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // logika pro výpis řady
+
+            int current = first;
+            while(current<= last){
+                Console.WriteLine(current);
+                current = current + step;
+            }
             Console.WriteLine("Pro opakování programu stiskněte klávesu a");
             again = Console.ReadLine();
         }
