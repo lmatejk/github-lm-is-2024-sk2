8c527263aefaefa887ab190882f6610cec7cdf09
Lucie Matějková <lucie.matejkova@tul.cz>
2024-10-10 17:51:31 +0200

    #1 projekt vypis rady

--- IS-Projekty/program001-vypis-rady/obj/Debug/net8.0/program001-vypis-rady.AssemblyInfo.cs
+++ IS-Projekty/program001-vypis-rady/obj/Debug/net8.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6708c87bccc45e9bc730ac5a643873945f7a54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce7e7856bd30da673ce0ef8c12a971193bf129c")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
