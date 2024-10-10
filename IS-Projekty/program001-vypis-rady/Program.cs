07bafc35d33ddd96567bea2e2ad2f134c30074eb
Lucie Matějková <lucie.matejkova@tul.cz>
2024-10-10 13:32:27 +0200

    #1 zakladni program s moznosti opakovani

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        
+        //chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a"){
+            Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("*********Výpis řady*******");
+            Console.WriteLine("**************************");
+            Console.WriteLine("******Lucie Matějková*****");
+            Console.WriteLine("**************************\n\n");     
+            Console.WriteLine();     
+            
+            //vstup od uživatele - TO-DO
+
+            //logika pro výpis řady - TO-DO
+
+            //opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }
+}
+
+
