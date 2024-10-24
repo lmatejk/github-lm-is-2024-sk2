3dad7e75d091f8ff59a3493bb3b1d92cdd6e1d06
Lucie Matějková <lucie.matejkova@tul.cz>
2024-10-24 16:44:43 +0200

    #4 generovani cisel - done

--- IS-Projekty/program004-generator/Program.cs
+++ IS-Projekty/program004-generator/Program.cs
@@ -1,57 +1,76 @@
 ﻿using System;
+using System.Security.AccessControl;
 using System.Security.Cryptography;
 
-class Program {
-    static void Main() {
-        
-        //chci, aby se program opakoval po stisku klávesy "a"
-        string again = "a";
-        while(again == "a"){
-            Console.Clear();
-            Console.WriteLine("***********************************************");
-            Console.WriteLine("*********Generátor pseudováhodných čísel*******");
-            Console.WriteLine("***********************************************");
-            Console.WriteLine("*****************Lucie Matějková***************");
-            Console.WriteLine("************************************************\n\n");     
-            Console.WriteLine();     
-            
-            Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
-            int n;
-            while(!int.TryParse(Console.ReadLine(),out n)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte znovu počet čísel (celé číslo): ");
-            }
 
-            Console.Write("Zadejte dolní mez (celé číslo): ");
-            int dm;
-            while(!int.TryParse(Console.ReadLine(),out dm)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte znovu dolní mez (celé číslo): ");
-            }
+//chci, aby se program opakoval po stisku klávesy "a"
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("***********************************************");
+    Console.WriteLine("*********Generátor pseudováhodných čísel*******");
+    Console.WriteLine("***********************************************");
+    Console.WriteLine("*****************Lucie Matějková***************");
+    Console.WriteLine("************************************************\n\n");
+    Console.WriteLine();
 
-            Console.Write("Zadejte horní mez (celé číslo): ");
-            int hm;
-            while(!int.TryParse(Console.ReadLine(),out hm)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte znovu horní mez (celé číslo): ");
-            }
-
-            Console.WriteLine("\n\n================");
-            Console.WriteLine("Uživatel zadal počet: {0}, dolní mez: {1}, horní mez: {2}",n, dm, hm);
-            Console.WriteLine("================\n\n");
-
-            //declare pole
-            int[]myArray = new int[n];
-
-            //příprava pro generování náhodných čísel
-            Random randomNumber = new Random();
-
-            Console.WriteLine("Náhodná čísla: ");
-            for(int i = 0; i < n;i++){
-                myArray[i] = randomNumber.Next(dm, hm);
-                Console.WriteLine("{0}", myArray[i]);
-            }
-
-
-            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
-            again = Console.ReadLine();
-        }
+    Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+    int n;
+    while (!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu počet čísel (celé číslo): ");
     }
+
+    Console.Write("Zadejte dolní mez (celé číslo): ");
+    int dm;
+    while (!int.TryParse(Console.ReadLine(), out dm))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu dolní mez (celé číslo): ");
+    }
+
+    Console.Write("Zadejte horní mez (celé číslo): ");
+    int hm;
+    while (!int.TryParse(Console.ReadLine(), out hm))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu horní mez (celé číslo): ");
+    }
+
+    Console.WriteLine("\n\n================");
+    Console.WriteLine("Uživatel zadal počet: {0}, dolní mez: {1}, horní mez: {2}", n, dm, hm);
+    Console.WriteLine("================\n\n");
+
+    //declare pole
+    int[] myArray = new int[n];
+
+    //příprava pro generování náhodných čísel
+    Random randomNumber = new Random();
+    int kladny = 0; int nulovy = 0; int zaporny = 0; int suda = 0; int licha = 0;
+
+    Console.WriteLine("Náhodná čísla: ");
+    for (int i = 0; i < n; i++)
+    {
+        
+
+        
+        myArray[i] = randomNumber.Next(dm, hm);
+        Console.WriteLine("{0}", myArray[i]);
+        if(myArray[i] > 0)
+            kladny++;
+        else if(myArray[i]<0)
+            zaporny++;
+        else
+            nulovy++;
+
+        if(myArray[i]%2==0)
+            suda++; 
+        else
+            licha++;           
+    }
+    Console.WriteLine("Kladný: {0}, nulový: {1}, záporný: {2}", kladny, nulovy, zaporny);
+    Console.WriteLine("Sudá: {0}, Lichá: {1}", suda,licha);
+
+
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
 }
--- IS-Projekty/program004-generator/obj/Debug/net8.0/program004-generator.AssemblyInfo.cs
+++ IS-Projekty/program004-generator/obj/Debug/net8.0/program004-generator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program004-generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afc58d03995764b604ecf0be43129023a6564b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4607f78fa1730c9f319f061cc44f19a4822496e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("program004-generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program004-generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
