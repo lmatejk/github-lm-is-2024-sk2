1afc58d03995764b604ecf0be43129023a6564b5
Lucie Matějková <lucie.matejkova@tul.cz>
2024-10-24 12:57:49 +0200

    #4 generátor - pseudonáhodných čísel

---
+++ IS-Projekty/program004-generator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        
+        //chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a"){
+            Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("*********Generátor*******");
+            Console.WriteLine("**************************");
+            Console.WriteLine("******Lucie Matějková*****");
+            Console.WriteLine("**************************\n\n");     
+            Console.WriteLine();     
+            
+
+
+            
+
+
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }
+}
